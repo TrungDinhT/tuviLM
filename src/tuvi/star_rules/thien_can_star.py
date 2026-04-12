c7ed7a2f7282a9d25aab92d0d2b70f05eafa615a
phuockhanhle <phuockhanhnk94@gmail.com>
2026-04-12 11:15:38 +0200

    fix vong loc ton

--- src/tuvi/star_rules/thien_can_star.py
+++ src/tuvi/star_rules/thien_can_star.py
@@ -15,61 +15,61 @@ def get_kinh_duong_position(locton_position: TYPE_DIA_CHI) -> TYPE_DIA_CHI:
     locton_index = LIST_DIA_CHI.index(locton_position)
     return LIST_DIA_CHI[(locton_index + 1) % 12]
 
-def get_thanh_long_position(locton_position: TYPE_DIA_CHI) -> TYPE_DIA_CHI:
+def get_thanh_long_position(locton_position: TYPE_DIA_CHI, direction: Literal[1, -1]) -> TYPE_DIA_CHI:
     locton_index = LIST_DIA_CHI.index(locton_position)
-    return LIST_DIA_CHI[(locton_index + 2) % 12]
+    return LIST_DIA_CHI[(locton_index + 2 * direction) % 12]
 
-def get_tieu_hao_position(locton_position: TYPE_DIA_CHI) -> TYPE_DIA_CHI:
+def get_tieu_hao_position(locton_position: TYPE_DIA_CHI, direction: Literal[1, -1]) -> TYPE_DIA_CHI:
     locton_index = LIST_DIA_CHI.index(locton_position)
-    return LIST_DIA_CHI[(locton_index + 3) % 12]
+    return LIST_DIA_CHI[(locton_index + 3 * direction) % 12]
 
 def get_ln_van_tinh_position(locton_position: TYPE_DIA_CHI) -> TYPE_DIA_CHI:
     locton_index = LIST_DIA_CHI.index(locton_position)
     return LIST_DIA_CHI[(locton_index + 3) % 12]
 
-def get_tuong_quan_position(locton_position: TYPE_DIA_CHI) -> TYPE_DIA_CHI:
+def get_tuong_quan_position(locton_position: TYPE_DIA_CHI, direction: Literal[1, -1]) -> TYPE_DIA_CHI:
     locton_index = LIST_DIA_CHI.index(locton_position)
-    return LIST_DIA_CHI[(locton_index + 4) % 12]
+    return LIST_DIA_CHI[(locton_index + 4 * direction) % 12]
 
-def get_tau_thu_position(locton_position: TYPE_DIA_CHI) -> TYPE_DIA_CHI:
+def get_tau_thu_position(locton_position: TYPE_DIA_CHI, direction: Literal[1, -1]) -> TYPE_DIA_CHI:
     locton_index = LIST_DIA_CHI.index(locton_position)
-    return LIST_DIA_CHI[(locton_index + 5) % 12]
+    return LIST_DIA_CHI[(locton_index + 5 * direction) % 12]
 
 def get_duong_phu_position(locton_position: TYPE_DIA_CHI) -> TYPE_DIA_CHI:
     locton_index = LIST_DIA_CHI.index(locton_position)
     return LIST_DIA_CHI[(locton_index + 5) % 12]
 
-def get_phi_liem_position(locton_position: TYPE_DIA_CHI) -> TYPE_DIA_CHI:
+def get_phi_liem_position(locton_position: TYPE_DIA_CHI, direction: Literal[1, -1]) -> TYPE_DIA_CHI:
     locton_index = LIST_DIA_CHI.index(locton_position)
-    return LIST_DIA_CHI[(locton_index + 6) % 12]
+    return LIST_DIA_CHI[(locton_index + 6 * direction) % 12]
 
-def get_hy_than_position(locton_position: TYPE_DIA_CHI) -> TYPE_DIA_CHI:
+def get_hy_than_position(locton_position: TYPE_DIA_CHI, direction: Literal[1, -1]) -> TYPE_DIA_CHI:
     locton_index = LIST_DIA_CHI.index(locton_position)
-    return LIST_DIA_CHI[(locton_index + 7) % 12]
+    return LIST_DIA_CHI[(locton_index + 7 * direction) % 12]
 
-def get_benh_phu_position(locton_position: TYPE_DIA_CHI) -> TYPE_DIA_CHI:
+def get_benh_phu_position(locton_position: TYPE_DIA_CHI, direction: Literal[1, -1]) -> TYPE_DIA_CHI:
     locton_index = LIST_DIA_CHI.index(locton_position)
-    return LIST_DIA_CHI[(locton_index + 8) % 12]
+    return LIST_DIA_CHI[(locton_index + 8 * direction) % 12]
 
 def get_quoc_an_position(locton_position: TYPE_DIA_CHI) -> TYPE_DIA_CHI:
     locton_index = LIST_DIA_CHI.index(locton_position)
     return LIST_DIA_CHI[(locton_index + 8) % 12]
 
-def get_dai_hao_position(locton_position: TYPE_DIA_CHI) -> TYPE_DIA_CHI:
+def get_dai_hao_position(locton_position: TYPE_DIA_CHI, direction: Literal[1, -1]) -> TYPE_DIA_CHI:
     locton_index = LIST_DIA_CHI.index(locton_position)
-    return LIST_DIA_CHI[(locton_index + 9) % 12]
+    return LIST_DIA_CHI[(locton_index + 9 * direction) % 12]
 
-def get_phuc_binh_position(locton_position: TYPE_DIA_CHI) -> TYPE_DIA_CHI:
+def get_phuc_binh_position(locton_position: TYPE_DIA_CHI, direction: Literal[1, -1]) -> TYPE_DIA_CHI:
     locton_index = LIST_DIA_CHI.index(locton_position)
-    return LIST_DIA_CHI[(locton_index + 10) % 12]
+    return LIST_DIA_CHI[(locton_index + 10 * direction) % 12]
 
 def get_da_la_position(locton_position: TYPE_DIA_CHI) -> TYPE_DIA_CHI:
     locton_index = LIST_DIA_CHI.index(locton_position)
     return LIST_DIA_CHI[(locton_index + 11) % 12]
 
-def get_quan_phu_position(locton_position: TYPE_DIA_CHI) -> TYPE_DIA_CHI:
+def get_quan_phu_position(locton_position: TYPE_DIA_CHI, direction: Literal[1, -1]) -> TYPE_DIA_CHI:
     locton_index = LIST_DIA_CHI.index(locton_position)
-    return LIST_DIA_CHI[(locton_index + 11) % 12]
+    return LIST_DIA_CHI[(locton_index + 11 * direction) % 12]
 
 def get_luc_si_position(locton_position: TYPE_DIA_CHI, direction: Literal[1, -1]) -> TYPE_DIA_CHI:
     locton_index = LIST_DIA_CHI.index(locton_position)
@@ -100,20 +100,20 @@ def get_star_by_thien_can_position(thien_can: TYPE_THIEN_CAN, direction: Literal
         ("Lộc Tồn", locton_position),
         ("Bác Sĩ", get_bac_si_position(locton_position)),
         ("Kình Dương", get_kinh_duong_position(locton_position)),
-        ("Thanh Long", get_thanh_long_position(locton_position)),
-        ("Tiểu Hao", get_tieu_hao_position(locton_position)),
+        ("Thanh Long", get_thanh_long_position(locton_position, direction)),
+        ("Tiểu Hao", get_tieu_hao_position(locton_position, direction)),
         ("LN Văn Tinh", get_ln_van_tinh_position(locton_position)),
-        ("Tướng Quân", get_tuong_quan_position(locton_position)),
-        ("Tấu Thư", get_tau_thu_position(locton_position)),
+        ("Tướng Quân", get_tuong_quan_position(locton_position, direction)),
+        ("Tấu Thư", get_tau_thu_position(locton_position, direction)),
         ("Đường Phù", get_duong_phu_position(locton_position)),
-        ("Phi Liêm", get_phi_liem_position(locton_position)),
-        ("Hỷ Thần", get_hy_than_position(locton_position)),
-        ("Bệnh Phù", get_benh_phu_position(locton_position)),
+        ("Phi Liêm", get_phi_liem_position(locton_position, direction)),
+        ("Hỷ Thần", get_hy_than_position(locton_position, direction)),
+        ("Bệnh Phù", get_benh_phu_position(locton_position, direction)),
         ("Quốc Ấn", get_quoc_an_position(locton_position)),
-        ("Đại Hao", get_dai_hao_position(locton_position)),
-        ("Phục Binh", get_phuc_binh_position(locton_position)),
+        ("Đại Hao", get_dai_hao_position(locton_position, direction)),
+        ("Phục Binh", get_phuc_binh_position(locton_position, direction)),
         ("Đà La", get_da_la_position(locton_position)),
-        ("Quan Phủ", get_quan_phu_position(locton_position)),
+        ("Quan Phủ", get_quan_phu_position(locton_position, direction)),
         ("Lực Sĩ", get_luc_si_position(locton_position, direction)),
         ("Lưu Hà", get_luu_ha_position(thien_can)),
         ("Thiên Trù", get_thien_tru_position(thien_can)),
