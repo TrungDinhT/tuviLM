9eeebc5eab01f9c4b66bc90f026ded83465c314f
phuockhanhle <phuockhanhnk94@gmail.com>
2026-07-11 12:27:58 +0200

    feat: fix import

--- src/agent/__init__.py
+++ src/agent/__init__.py
@@ -1,9 +1,21 @@
 from .deps import TuviAgentDeps
 from .main import DEFAULT_MODEL, build_tuvi_agent, run_tuvi_agent
+from src.agent.personality_workflow import (
+    PersonalityEvidence,
+    build_personality_agent,
+    collect_personality_evidence,
+    get_personality_evidence,
+    run_personality_workflow,
+)
 
 __all__ = [
     "DEFAULT_MODEL",
+    "PersonalityEvidence",
     "TuviAgentDeps",
+    "build_personality_agent",
     "build_tuvi_agent",
+    "collect_personality_evidence",
+    "get_personality_evidence",
+    "run_personality_workflow",
     "run_tuvi_agent",
 ]
